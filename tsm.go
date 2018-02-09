c6ec1c48287f8a7e2ad2db23b96c479395e5a76d
emersion <contact@emersion.fr>
2018-02-09 09:25:37 +0100

    Add a few Screen functions

--- tsm.go
+++ tsm.go
@@ -22,3 +22,31 @@ func NewScreen() *Screen {
 	})
 	return s
 }
+
+func (s *Screen) Width() uint {
+	return uint(C.tsm_screen_get_width(s.s))
+}
+
+func (s *Screen) Height() uint {
+	return uint(C.tsm_screen_get_height(s.s))
+}
+
+func (s *Screen) Resize(w, h uint) {
+	if ret := C.tsm_screen_resize(s.s, C.uint(w), C.uint(h)); ret != 0 {
+		panic("tsm: cannot resize screen")
+	}
+}
+
+func (s *Screen) SetMargins(top, bottom uint) {
+	if ret := C.tsm_screen_resize(s.s, C.uint(top), C.uint(bottom)); ret != 0 {
+		panic("tsm: cannot set screen margins")
+	}
+}
+
+func (s *Screen) SetMaxScrollback(max uint) {
+	C.tsm_screen_set_max_sb(s.s, C.uint(max))
+}
+
+func (s *Screen) ClearScrollback() {
+	C.tsm_screen_clear_sb(s.s)
+}
